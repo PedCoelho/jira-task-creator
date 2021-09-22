50cf8cd06b0559bbcf147dcf0e6a56ef29cf7499
PedCoelho <cabralcoelho@gmail.com>
2021-09-22 10:03:24 -0300

    [MINOR POPUP UPDATES]

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 import { setUser } from "./js/userHelpers.js";
 import { checkUrl, getProjKey } from "./js/urlHelpers.js";
-
-const selectEl = (selector) => document.querySelector(selector);
+import { selectEl } from "./js/utils.js";
 
 let userEl = selectEl("#username");
 let disclaimer = selectEl("#bad-page");
@@ -21,16 +20,16 @@ function showForm() {
 /*                      stop execution if this step fails                     */
 /* -------------------------------------------------------------------------- */
 let url_check = checkUrl();
-if (!url_check)
+if (!url_check) {
   throw new Error("Wrong page address, or address couldn`t be verified");
-
+}
 /* -------------------------------------------------------------------------- */
 /*                   else, get/set user and show file input                   */
 /* -------------------------------------------------------------------------- */
 
 showForm();
 
+//todo move to form builder???
 let projectKey = getProjKey();
 selectEl("input[name=projectKey]").value = projectKey;
-
 console.log(projectKey);
