72e1c470d10b2b05acaadacc6c646aea392f7db2
pedro.silva <pedro.silva@mjv.com.br>
2021-11-09 13:27:15 -0300

    [MADE URL CHECK MORE GENERIC]

--- js/urlHelpers.js
+++ js/urlHelpers.js
@@ -6,15 +6,11 @@ async function getCurrentTab() {
 
 const currentTab = await getCurrentTab();
 
-let match = (url) =>
-  Boolean(url.match(/\.atlassian.net\/secure\/RapidBoard\.jspa/, "g"));
+let match = (url) => Boolean(url.match(/secure\/RapidBoard\.jspa/, "g"));
 
 export const getProjKey = () =>
   new URL(currentTab.url).searchParams.get("projectKey");
 
 export const checkUrl = () => {
-  //   return getCurrentTab().then((res) => {
-  // let url = res.url;
   return match(currentTab.url);
-  //   });
 };
